7008d61dc15512b3aa34d3929b1f0e4a496560cb
dbPieter <accounts_pdb@protonmail.com>
2024-05-11 15:55:26 +0200

    Initial Commit

---
+++ src/main/kotlin/be/dbpieter/utiloverrest/UtilOverRestApplication.kt
@@ -0,0 +1,11 @@
+package be.dbpieter.utiloverrest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UtilOverRestApplication
+
+fun main(args: Array<String>) {
+	runApplication<UtilOverRestApplication>(*args)
+}
---
+++ src/test/kotlin/be/dbpieter/utiloverrest/UtilOverRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package be.dbpieter.utiloverrest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UtilOverRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
